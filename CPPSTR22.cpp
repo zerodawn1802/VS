28af07ddf67de8ba5dd45d1e3a36f28d5063940f
zerochan1802 <phuongacma1802@gmail.com>
2020-12-03 23:00:23 +0700

    update

--- CPPSTR22.cpp
+++ CPPSTR22.cpp
@@ -15,7 +15,7 @@ int main()
         for(int i = 0; i < s1.size(); i++)
 		{
 			if(s1[i] != ' ' && s1[i] != '\t' && i != s1.size() - 1) tmp += s1[i];
-			else if(i == s1.size() - 1)
+			else if(i == s1.size() - 1 && s1[i] != ' ' && s1[i] != '\t')
 			{
 				tmp += s1[i];
 				if(tmp != "") a.push_back(tmp);
@@ -30,7 +30,7 @@ int main()
         for(int i = 0; i < s2.size(); i++)
 		{
 			if(s2[i] != ' ' && s2[i] != '\t' && i != s2.size() - 1) tmp += s2[i];
-			else if(i == s2.size() - 1)
+			else if(i == s2.size() - 1 && s2[i] != ' ' && s2[i] != '\t')
 			{
 				tmp += s2[i];
 				if(tmp != "") b.push_back(tmp);
@@ -55,26 +55,30 @@ int main()
             }
             if(kt == 1) c.push_back(a[i]);
         }
-        for(int i = 0; i < c.size() - 1; i++)
+        if(c.empty());
+        else
         {
-            for(int j = i + 1; j < c.size(); j++)
-            {
-                if(c[i] > c[j])
-                {
-                    string tmp = c[i];
-                    c[i] = c[j];
-                    c[j] = tmp;
-                }
-            }
-        }
-        d.push_back(c[0]);
-        for(int i = 1; i < c.size(); i++)
-        {
-            if(c[i] != d[d.size() - 1]) d.push_back(c[i]);
-        }
-        for(int i = 0; i < d.size(); i++)
-        {
-            cout << d[i] << " ";
+        	for(int i = 0; i < c.size() - 1; i++)
+        	{
+            	for(int j = i + 1; j < c.size(); j++)
+            	{
+                	if(c[i] > c[j])
+                	{
+                    	string tmp = c[i];
+                    	c[i] = c[j];
+                    	c[j] = tmp;
+                	}
+            	}
+        	}
+        	d.push_back(c[0]);
+        	for(int i = 1; i < c.size(); i++)
+        	{
+        	    if(c[i] != d[d.size() - 1]) d.push_back(c[i]);
+        	}
+        	for(int i = 0; i < d.size(); i++)
+        	{
+        	    	cout << d[i] << " ";
+        	}
         }
         cout << endl;
     }
